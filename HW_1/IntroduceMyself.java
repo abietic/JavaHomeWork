06b5e9d250b62beaeab8d7ac52124abad2ce6f4d
abietic <iamyhc@live.cn>
2017-10-15 12:54:57 +0800

    Create IntroduceMyself.java

---
+++ HW_1/IntroduceMyself.java
@@ -0,0 +1,13 @@
+
+public class IntroduceMyself {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String age = "19 years old.";
+		String height = "1.61 meters tall.";
+		String weight = "51 kilograms weight.";
+		System.out.println("My name is Yu Haichuan." + age + height + weight);
+	}
+
+}
