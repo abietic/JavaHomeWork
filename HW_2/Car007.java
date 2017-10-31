d1ce12362d14daf326c72b47f789ca4cd133673e
abietic <iamyhc@live.cn>
2017-10-31 19:32:33 +0800

    Create Car007.java

---
+++ HW_2/Car007.java
@@ -0,0 +1,12 @@
+
+public class Car007 implements Common{
+
+	public int calculate(int A , int B , int C) {
+		return A * B / C;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
